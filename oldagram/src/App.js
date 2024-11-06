1b8b605b56e74c5a00675bfdabbf0df644fe3413
Salman Noushad <salmannoushad003@gmail.com>
2024-11-06 22:42:09 +0530

    scrimba

---
+++ oldagram/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div id="Card">
+      <div id="card-title"></div>
+      <div id="card-post"></div>
+      <div id="card-status"></div>
+      <div id="card-caption"></div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
